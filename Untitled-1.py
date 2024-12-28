bb9f88ec6d4c2679eefb0f48787b73abecde6f80
vatikalashashank <vatikalashashank@gmail.com>
2024-12-28 10:37:51 +0530

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,54 @@
+# for x in range(3):
+#     for y in range(10):
+#         print("*")
+
+# for x in range(3):
+#     for y in range(10):
+#         print("*",end='')
+
+# for row in range(4):
+#     for cal in range (5):
+#         print("*",end=' ')
+#     print()
+
+# for row in range(4):
+#     for col in range(row+1):
+#         print("*",end=' ')
+#     print()
+
+# for row in range (4,0,-1):
+#     for col in range(row):
+#         print("*",end=' ')
+#     print()
+
+# for row in range(1,6):
+#     for col in range(row):
+#         print("*",end=' ')
+#     print()
+
+# for row in range (5):
+#     for col in range(row+1):
+#         print("*",end=' ')
+#     print()
+# for row in range (4):
+#     for col in range(4-row):
+#         print("*",end=' ')
+#     print()
+
+# for row in range (5):
+#     for col in range(row+1):
+#         print(row,end=' ')
+#     print()
+# for row in range (5):
+#     for col in range(row+1):
+#         print(col,end=' ')
+#     print()
+
+# val=[1,87,79]
+# for x in range(len(val)):
+#     for y in range (len(val)):
+#         if val[x]<val[y]:
+#             val[x],val[y]=val[y],val[x]
+#         else:
+#             val[x],val[y]=val[x],val[y]
+# print(val)
